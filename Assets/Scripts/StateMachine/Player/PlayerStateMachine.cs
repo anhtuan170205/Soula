332a7db46aa24b7bfb4ba3a849de3f85349812d8
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-08-30 16:59:44 +0700

    Input Event

--- Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -2,19 +2,10 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
-    private float m_duration = 5f;
+    [field: SerializeField] public InputReader InputReader { get; private set; }
     private void Start()
     {
         SwitchState(new PlayerTestState(this));
     }
 
-    private void Update()
-    {
-        m_duration -= Time.deltaTime;
-        Debug.Log(m_duration);
-        if (m_duration <= 0f)
-        {
-            SwitchState(new PlayerTestState(this));
-        }
-    }
 }
--- Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) {}
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+    private float m_timer;
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        m_stateMachine.InputReader.JumpEvent += OnJump;
     }
 
     public override void Tick(float deltaTime)
     {
-        Debug.Log("Tick");
+        m_timer += deltaTime;
+        Debug.Log(m_timer);
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        m_stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        m_stateMachine.SwitchState(new PlayerTestState(m_stateMachine));
     }
 }
