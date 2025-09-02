52c71926499feddcae1f06b47d3b12f796470b18
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-09-02 21:54:55 +0700

    Movement Blend Tree

--- Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -10,11 +10,20 @@ public class PlayerTestState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
+        if (m_stateMachine.InputReader.MovementValue == Vector2.zero)
+        {
+            m_stateMachine.Animator.SetFloat("FreeLookSpeed", 0, 0.1f, deltaTime);
+            return;
+        }
+
+        m_stateMachine.Animator.SetFloat("FreeLookSpeed", 1, 0.1f, deltaTime);
         Vector3 movement = new Vector3();
         movement.x = m_stateMachine.InputReader.MovementValue.x;
         movement.z = m_stateMachine.InputReader.MovementValue.y;
         movement.y = 0;
-        m_stateMachine.transform.Translate(movement * deltaTime);
+
+        m_stateMachine.Controller.Move(movement * m_stateMachine.FreeLookMoveSpeed * deltaTime);
+        m_stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
 
     public override void Exit()
