e8d96607e0870a90f19be4e24974c4a2eb48277b
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-09-03 12:42:06 +0700

    Camera Relative Movement

--- Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -5,10 +5,12 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public InputReader InputReader { get; private set; }
     [field: SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public Animator Animator { get; private set; }
-
     [field: SerializeField] public float FreeLookMoveSpeed { get; private set; }
+    public Transform MainCameraTransform { get; private set; }
+     
     private void Start()
     {
+        MainCameraTransform = Camera.main.transform;
         SwitchState(new PlayerTestState(this));
     }
 
--- Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -17,16 +17,27 @@ public class PlayerTestState : PlayerBaseState
         }
 
         m_stateMachine.Animator.SetFloat("FreeLookSpeed", 1, 0.1f, deltaTime);
-        Vector3 movement = new Vector3();
-        movement.x = m_stateMachine.InputReader.MovementValue.x;
-        movement.z = m_stateMachine.InputReader.MovementValue.y;
-        movement.y = 0;
-
+        Vector3 movement = CalculateMovement();
+        
         m_stateMachine.Controller.Move(movement * m_stateMachine.FreeLookMoveSpeed * deltaTime);
         m_stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
 
     public override void Exit()
     {
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = m_stateMachine.MainCameraTransform.forward;
+        Vector3 right = m_stateMachine.MainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * m_stateMachine.InputReader.MovementValue.y + right * m_stateMachine.InputReader.MovementValue.x;
+
     }
 }
