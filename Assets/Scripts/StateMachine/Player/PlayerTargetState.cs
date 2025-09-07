7b603b478274f12eace77b73859d7b59af4fdbf6
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-09-07 20:27:01 +0700

    State Driven Camera

--- Assets/Scripts/StateMachine/Player/PlayerTargetState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerTargetState.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PlayerTargetState : PlayerBaseState
 {
+    private readonly int TargetBlendTreeHash = Animator.StringToHash("TargetBlendTree");
     public PlayerTargetState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
         m_stateMachine.InputReader.CancelEvent += OnCancel;
+        m_stateMachine.Animator.Play(TargetBlendTreeHash);
     }
 
     public override void Tick(float deltaTime)
